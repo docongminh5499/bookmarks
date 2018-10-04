019d67b0a1d9d7a4d32b58323cca814d6208441e
Minh <meo123zzz@gmail.com>
2018-10-04 21:13:16 +0700

    ver8

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y)3wv&^6$_ld*jc4jq7uk8m46+6q67hu_^&cl__g(kmv^x!7@i'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['minhdo5499.pythonanywhere.com']
+ALLOWED_HOSTS = ['minhdo5499.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
