5137ac71c1efa100269c5c479652f1a78d568b09
Minh <meo123zzz@gmail.com>
2018-10-05 15:32:10 +0700

    ver10

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, re_path, include
 from django.conf.urls import url
+from django.contrib.auth import views as auth_view
 from . import views
 
 urlpatterns = [
-		path('login/', views.user_login, name = 'login')
-		#url(r'login', login)
+		#path('login/', views.user_login, name = 'login')
+		url(r'login', auth_view.LoginView.as_view(template_name = 'account/login.html'), name = 'login')
 ]
