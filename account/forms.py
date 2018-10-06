b26fc8c9aadef7e254f7aa2bb92e387ede137c58
Minh <meo123zzz@gmail.com>
2018-10-06 15:30:24 +0700

    ver15

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
-class LoginForm (forms.Form):
-	username = forms.CharField()
-	password = forms.CharField(widget = forms.PasswordInput)
+class LoginForm (AuthenticationForm):
+	username = forms.CharField(widget = forms.TextInput(attrs = {'placeholder': 'Username'}))
+	password = forms.CharField(widget = forms.PasswordInput(attrs = {'placeholder': 'Password'}))
+
+
+
--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,15 @@
-from django.urls import path, re_path, include
+from django.urls import path, re_path, include, reverse_lazy
 from django.conf.urls import url
 from django.contrib.auth import views as auth_view
 from . import views
+from .forms import LoginForm
 
 urlpatterns = [
 		#path('login/', views.user_login, name = 'login')
-		url(r'login', auth_view.LoginView.as_view(template_name = 'account/login.html'), name = 'login'),
+		url(r'login', auth_view.LoginView.as_view(template_name = 'account/login.html', authentication_form = LoginForm), name = 'login'),
 		url(r'logout', auth_view.LogoutView.as_view(template_name = 'account/logout.html'), name = 'logout'),
 		url(r'dashboard', views.dashboard, name = 'dashboard'),
-		
+		url(r'password_change', auth_view.PasswordChangeView.as_view(template_name = 'account/password_change.html', success_url = reverse_lazy('account:password_change_done')), name = 'password_change'),
+		url(r'password_change_done', auth_view.PasswordChangeDoneView.as_view(template_name = 'account/password_change_done.html'), name = 'password_change_done')
+
 ]
